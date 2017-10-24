cd330e246d5faff1cfedc6656a7150cf4282c81b
Liudmyla Iefremova <liudmyla.iefremova@gmail.com>
2017-10-24 14:39:43 +0200

    working on the Home page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,51 +1,78 @@
 <template>
-  <div id="app">
-    <nav class="navbar is-primary" role="navigation" aria-label="main navigation" >
-      <div class="navbar-end">
-        <router-link v-if="!$root.user" to="/login" class="navbar-item">
-            Login
-        </router-link>
-        <router-link v-if="!$root.user" to="/signup" class="navbar-item">
-            Signup
-        </router-link>
-        <a class="navbar-item" @click.prevent="logout" v-if="$root.user" href="#">
-            Logout
+	<div id="app">
+		<nav class="navbar is-primary" role="navigation" aria-label="main navigation" >
+			<div class="navbar-brand">
+				<router-link class="navbar-item" to="/">
+					Sovenirs Delivery
+				</router-link>
+        <a class="gh-btn navbar-item" title="Github" id="gh-btn" target="_blank" aria-label="Star on GitHub" href="https://github.com/LiudmilaE/ironhack-project3-souvenirs-delivery">
+          <i class="fa fa-github" aria-hidden="true"></i>
         </a>
-      </div>
-    </nav>
-    <div class="container">
-      <router-view/>
-    </div>
-  </div>
+        
+
+				<div class="navbar-burger burger" data-target="navMenu">
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+			</div>
+
+			<div id="navMenu" class="navbar-menu">
+				<div class="navbar-end">
+					<router-link v-if="!$root.user" to="/login" class="navbar-item">
+							Login
+					</router-link>
+					<router-link v-if="!$root.user" to="/signup" class="navbar-item">
+							Signup
+					</router-link>
+					<a class="navbar-item" @click.prevent="logout" v-if="$root.user" href="#">
+							Logout
+					</a>
+				</div>
+			</div>
+		</nav>
+		<div>
+			<router-view/>
+		</div>
+		<footer class="footer">
+			<div class="container">
+		    <div class="content has-text-centered">
+		      <p> &copy;
+		        <strong>Souvenir Delivery</strong> by <a href="https://www.linkedin.com/in/liudmyla-iefremova/" target="_blank">Liudmyla Iefremova</a>. The source code is available on
+		        <a href="https://github.com/LiudmilaE/ironhack-project3-souvenirs-delivery" target="_blank"><i class="fa fa-github" aria-hidden="true"></i> GitHub</a>.
+		      </p>
+		    </div>
+		  </div>
+		</footer>
+	</div>
 </template>
 
 <script>
 import { logout } from '@/api/auth'
 
 export default {
-  name: 'app',
-  methods: {
-    logout () {
-      logout(this.$root);
-      this.$router.push('/');
-    }
-  }
+	name: 'app',
+	methods: {
+		logout () {
+			logout(this.$root);
+			this.$router.push('/');
+		}
+	}
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 #app {
-  margin-top: 0;
+	margin-top: 0;
 }
 .container {
-  padding-top: 40px;
+	padding-top: 40px;
 }
 </style>
--- client/src/router/Home.vue
+++ client/src/router/Home.vue
@@ -1,6 +1,39 @@
 <template>
 	<div>
-		<h1>Home</h1>
-		<p v-if="$root.user">Hi {{ $root.user.name }}</p>
+		<section class="hero is-primary is-medium">
+			<div class="hero-body" id="img-hero">
+				<div class="container">
+					<h1 class='title'>Souvenir Delivery</h1>
+					<h2 class="subtitle">There is always something tasty to discover in any country!</h2>
+				</div>
+				<p  v-if="$root.user">Hi, {{ $root.user.firstName }}!</p>
+			</div>
+		</section>
+		<section class="hero is-primary">
+			<div class="hero-body">
+				<div class="container">
+					<h2 class="title">
+						Earn extra income
+					</h2>
+					<h3 class="subtitle">
+						Register your spare space in baggage while traveling, bring tasty food from your hometown and earn some extra income
+					</h3>
+					<button class="button is-primary is-large is-inverted is-outlined" id="trip-btn">Register your trip</button>
+				</div>
+			</div>
+		</section>
+			
 	</div>
 </template>
+
+<style scoped>
+ #img-hero {
+	background-image: url('../assets/globe-map-world-XL.jpg');
+	background-repeat: cover;
+	background-position: center;
+ }
+ #trip-btn {
+ 	margin: 10px 45%;
+ }
+
+</style>
