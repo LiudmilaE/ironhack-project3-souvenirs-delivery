306fadbe59737ec9c044ace0ed945dd403e49895
Liudmyla Iefremova <liudmyla.iefremova@gmail.com>
2017-10-28 13:23:05 +0200

    added some styles and content

--- client/src/router/About.vue
+++ client/src/router/About.vue
@@ -1,17 +1,40 @@
 <template>
-	
+	<div>
 		<section class="hero is-info is-medium has-text-centered">
 			<div class="hero-body" id="img-about">
 				<div class="container">
 					<h2 class="title is-1">How does it work?</h2>
 				</div>
-				
 			</div>
-			
 		</section>
-		
 
-
+		<section class="container">
+			<div class="columns is-vcentered">
+				<div class="column is-6">
+					<p class="title has-text-centered">Going to travel?</p>
+					<ol>
+						<li>Search for a traveler</li>
+						<li>Book free space in traveler's baggage</li>
+						<li>Schedule the pick up</li>
+						<li>Pick up your order</li>
+						<li>Enjoy tasty food</li>
+					</ol>
+					<router-link to="/trips"><span class="button is-primary is-large is-outlined">Make an order</span></router-link>
+				</div>
+				<div class="column is-6">
+					<p class="title has-text-centered">Looking for a special food?</p>
+					<ol>
+						<li>Register your travel</li>
+						<li>Get baggage sharing requests</li>
+						<li>Schedule your apointment with client</li>
+						<li>Meet the client</li>
+						<li>Earn money</li>
+					</ol>
+					<router-link to="/trips/new"><span class="button is-primary is-large is-outlined">Register your new trip</span></router-link>
+				</div>
+			</div>
+		</section>
+	</div>	
 </template>
 
 <script>
@@ -29,9 +52,7 @@
 /*	padding-bottom: 10rem;
  	padding-top: 10rem;*/
  }
-
  h2 {
  	text-shadow: 1px 1px 2px grey; 
  }
-
 </style>
