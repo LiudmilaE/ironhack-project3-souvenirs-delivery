5aeb160ea8a6ded7f90d78071934c1fdcc3b1c1c
Liudmyla Iefremova <liudmyla.iefremova@gmail.com>
2017-10-30 19:50:05 +0100

    sketch for seeds

--- server/bin/seeds.js
+++ server/bin/seeds.js
@@ -6,3 +6,16 @@ const Souvenir = require('../models/souvenir');
 mongoose.connect('mongodb://localhost/souvenirs-delivery', {
 	useMongoClient: true,
 });
+
+//4 functions:
+//connection to DB
+//cleanup
+//seeding
+//disconnect
+
+/*function findIdWithProperty(array, property, value){
+	const el = array.find(el=> el[property]===value)
+	if (!el) {throw Error('Cannot find object) }
+	return el._id
+}
+F.E.  findIdWithProperty(master, 'title', 'Asterix & Obelix')*/
